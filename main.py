09efb3a87426285f357f3367c4000d345ff04cbb
omar-7077 <amoor6163@gmail.com>
2025-05-15 14:35:27 +0300

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKe
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, CallbackQueryHandler, ContextTypes, filters
 from datetime import datetime
 
-TOKEN = "ضع_التوكن_هنا"
+TOKEN = "7597887705:AAEQr0g_aWxoZb6o1QC5geKZ3GzCBQtl7fY"
 
 main_menu = [
     ["موعد المكافأة", "أرقام التواصل"],
